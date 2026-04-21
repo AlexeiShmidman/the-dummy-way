adaba9a2605904d1a7697ee4e1b38d15eede4a04
Alexei Shmidman <alexeishmidman@gmail.com>
2026-04-21 21:57:21 +0300

    Implement chat memory access in ChatAssistant and clear memory on disconnect. Fix reconnect after disconnect.

--- quarkus-ollama-imperative-integration/src/main/java/com/thedummyway/imperative/ai/ChatAssistant.java
+++ quarkus-ollama-imperative-integration/src/main/java/com/thedummyway/imperative/ai/ChatAssistant.java
@@ -2,10 +2,11 @@ package com.thedummyway.imperative.ai;
 
 import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.memory.ChatMemoryAccess;
 import io.quarkiverse.langchain4j.CreatedAware;
 
 @CreatedAware
-public interface ChatAssistant {
+public interface ChatAssistant extends ChatMemoryAccess {
 
   String chat(@MemoryId Object memoryId, @UserMessage String userMessage);
 }
